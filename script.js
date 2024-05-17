eca2066f38761758bbd945c0c396b74a6348380b
Dmartinez324 <109042989+Dmartinez324@users.noreply.github.com>
2024-05-17 01:55:00 -0600

    First Publish

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+let images = [
+    "assets/LinkedinPhoto.png",
+    "assets/Horses.png",
+    "assets/SonWithMother.png",
+    "assets/LakePicture.png"
+]
+
+
+let index = 0;
+const imgElement = document.querySelector('#slider-img');
+
+function change() {
+   imgElement.src = images[index];
+   index > 2 ? index = 0 : index++;
+}
+
+window.onload = function () {
+    setInterval(change, 2000);
+};
